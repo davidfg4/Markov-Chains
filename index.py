cc7c7a1888ffa933b90ec610ad8e3918314817af
David Gianforte <david.gianforte@gmail.com>
2013-10-14 10:44:14 -0600

    create login page

--- index.py
+++ index.py
@@ -5,8 +5,14 @@ import markovChains
 
 def main():
     markovChains.printHeader()
-    print("<h1>nothing yet!</h1>")
-    print("This will probably be the login page")
+    print("""<form method="get" action="user.py">
+Name: <input type="text" size="20" maxlength="40" name="name"> <br />
+Password: <input type="password" size="20" maxlength="40" name="name"> <br />
+<input type="submit" value="Log in">
+</form>
+<br />
+<a href="create_account.py">Create account</a>
+""")
     markovChains.printFooter()
 
 
